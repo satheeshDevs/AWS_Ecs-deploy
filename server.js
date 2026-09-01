9b0dacce928a5a6ec7a7c8c02ca53f7fc99d3e87
Satheesh-74 <satheeshkumar.a@vuedata.in>
2026-09-01 22:42:26 +0530

    inital-commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "healthy"
+  });
+});
+
+app.get("/api/hello", (req, res) => {
+  res.json({
+    message: "Hello from ECS!"
+  });
+});
+
+app.get("/api/users", (req, res) => {
+  res.json({
+    users: [
+      { id: 1, name: "John" },
+      { id: 2, name: "David" }
+    ]
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
