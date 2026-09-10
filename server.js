dc00091aef8c78fe159b236d99c79c28b3cb5932
Satheesh-74 <satheeshkumar.a@vuedata.in>
2026-09-10 14:31:36 +0530

    test automatic trigger

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.get("/api/users", (req, res) => {
   res.json({
     users: [
       { id: 1, name: "John" },
-      { id: 2, name: "Davidkumwarss" },
+      { id: 2, name: "Davidksssumwarss" },
     ],
   });
 });
